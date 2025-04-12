684ae07dbc42a5c6fbd3dcfd6bea82371c559d46
Ajay Kumar <ajaysharadkumar@gmail.com>
2025-04-12 23:29:20 +0530

    updating the git integration on nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,4 +1,26 @@
 return {
-    { "lewis6991/gitsigns.nvim" },  -- Git signs in the gutter
-  }
-  
+  -- Inline Git indicators in the gutter
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+
+  -- Full Git integration via commands
+  {
+    "tpope/vim-fugitive"
+  },
+
+  -- Lazygit integration
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<CR>", desc = "Open Lazygit" }
+    },
+    config = function()
+      vim.g.lazygit_floating_window_winblend = 0 -- transparency
+    end,
+  },
+}
